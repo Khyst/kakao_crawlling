94a8c10679758b630da10bf7d6c18a59e4a8a0a1
Khyst <kyh9432242@likelion.org>
2019-07-26 17:27:29 +0900

    LLLast

--- blog/models.py
+++ blog/models.py
@@ -7,9 +7,3 @@ class Blog(models.Model):
     pub_date = models.DateTimeField('date published')
     body = models.TextField()
 
-for i in range(1, 10, 1):
-    myfake = Faker()
-    blogger = Blog()
-    blogger.title = myfake.name
-    blogger.pub_date = myfake.date
-    blogger.body = myfake.text
