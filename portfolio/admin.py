f56550ebf49b6c42c9e64f000508cada7b80dd65
Khyst <kyh9432242@likelion.org>
2019-09-02 16:40:13 +0900

    first

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from .models import Portfolio
+from rangefilter.filter import DateRangeFilter, DateTimeRangeFilter  # 모듈 불러오기
+
 
 # Register your models here.
-admin.site.register(Portfolio)
+# admin.py
+class PortfolioAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'title', 'image', 'description']
+    """list_filter = ('gender',
+        ('hire_date', DateRangeFilter),
+        ('birth_date', DateRangeFilter),
+    )"""
+    search_fields = ['title', 'description']
+
+#admin.site.register(Portfolio)
+admin.site.register(Portfolio, PortfolioAdmin)
+
+
